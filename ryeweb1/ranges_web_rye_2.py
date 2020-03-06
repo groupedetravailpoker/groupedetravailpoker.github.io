1016874bf4af42e9f84a720843a7370f39f7e9d4
groupedetravailpoker <59604853+groupedetravailpoker@users.noreply.github.com>
2020-03-06 12:48:02 +0100

    Add files via upload

---
+++ ryeweb1/ranges_web_rye_2.py
@@ -0,0 +1,60 @@
+from os import listdir
+from os.path import isfile, join
+import json
+
+ranges_dir = "ryeweb"
+refs_dir = join("/home/vazqmig/Files/pok/env/src/ranges_web/", ranges_dir)
+dirs = [f for f in listdir(refs_dir) if not isfile(join(refs_dir, f))]
+
+
+# structure of the files:
+# > flat3bet
+#  >  20bb
+#       flat3bet 20bb bu utg-mp
+#        action  stack hero villain
+
+dirs
+# ['50 BB Flat et 3Bet', 'BB defending', 'Openraising 10bb push', 'OR - 60bb', 'OR -20bb', 'OR -30bb', 'OR -40bb', 'OR -100bb', 'OR SB 40bb+', '20bb flat 3bet', '30bb Flat 3bet', '40 BB Flat et 3Bet']
+
+rfiles = []
+for dr in dirs:
+    files = [f for f in listdir(join(refs_dir, dr))
+             if isfile(join(refs_dir, dr, f))]
+    for fname in files:
+        fl = join(ranges_dir, dr, fname)
+        props = {}
+        props['dir'] = dr
+        props['basename'] = fname
+        props['filename'] = fl
+        fn = fname.replace('_', ' ')
+        fn = fn.upper()
+        fn = fn.replace('.JPG', '')
+        fn = fn.replace('.PNG', '')
+        split = dr.split(' ')
+        ll = len(split)
+        props['label'] = fn
+        props['action'] = split[0] if ll > 0 else ''
+        props['stack'] = split[1] if ll > 1 else ''
+        if props['action'] == 'OR':
+            fn = fn.split(' ')[0]
+            props['label'] = fn
+        hero = fn
+        vil = ''
+        if props['action'] == 'Flat3bet':
+            hero = fn.split(' ')[0]
+            vil = fn.split(' ')[2]
+        props['hero'] = hero
+        props['vil'] = vil
+        #if props['action'] == 'BB':
+            #props['action'] = 'BB defense'
+            #props['stack'] = props['label']
+ 
+        rfiles.append(props)
+
+#print(rfiles
+print(json.dumps(rfiles, indent=4, sort_keys=True))
+
+file_path = join(refs_dir, ranges_dir, 'ranges_rye_2.json')
+
+with open(file_path, 'w') as outfile:
+    json.dump(rfiles, outfile, indent=4, sort_keys=True)
